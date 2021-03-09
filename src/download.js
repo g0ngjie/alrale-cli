2efc8754f7d955460ea9eb02d9ee75b9155a1c04
Gj <gongjie0422@163.com>
2021-03-09 18:25:44 +0800

    fix: modify;

--- src/download.js
+++ src/download.js
@@ -21,18 +21,18 @@ function downloadTemplate(url, target) {
 /**
  * 下载koa2模板
  */
-function koaBasicServices() {
+function koaBasicServices(custom) {
     const url = 'https://gitee.com:gjwork/koa2-basic-services-template#master'
-    const dir = 'koa2-basic-services-template'
+    const dir = custom === '[default]' ? 'koa2-basic-services-template' : custom
     return downloadTemplate(url, dir)
 }
 
 /**
  * 下载vue2-element-ui模板
  */
-function vue2BasicElementUI() {
+function vue2BasicElementUI(custom) {
     const url = 'https://gitee.com:gjwork/vue2-basic-element-ui-template#master'
-    const dir = 'vue2-basic-element-ui-template'
+    const dir = custom === '[default]' ? 'vue2-basic-element-ui-template' : custom
     return downloadTemplate(url, dir)
 }
 
@@ -40,23 +40,30 @@ function vue2BasicElementUI() {
  * 模板下载
  */
 exports.InitTemplate = async function () {
-    const { ok, msg, data } = await util.Inquirer([{
-        type: 'list',
-        message: '模板选择',
-        name: 'template',
-        choices: ["koa2-basic-template", "vue2-basic-element-ui-template"],
-    }])
+    const { ok, msg, data } = await util.Inquirer([
+        {
+            type: 'list',
+            message: '模板选择',
+            name: 'template',
+            choices: ["koa2-basic-template", "vue2-basic-element-ui-template"],
+        }, {
+            type: 'input',
+            message: '模板名称',
+            name: 'custom',
+            default: '[default]'
+        }
+    ])
 
     if (!ok) return print.Error(msg)
     const spinner = ora('Loading...').start();
     spinner.color = 'cyan';
     spinner.text = '正在安装...';
-    const { template } = data;
+    const { template, custom } = data;
     let successMsg = []
     // 下载koa模板
     switch (template) {
         case 'koa2-basic-template':
-            const koa2_path = await koaBasicServices()
+            const koa2_path = await koaBasicServices(custom)
             successMsg = [
                 `cd ${koa2_path}`,
                 'yarn 或者 npm install 安装依赖',
@@ -64,7 +71,7 @@ exports.InitTemplate = async function () {
             ]
             break;
         case 'vue2-basic-element-ui-template':
-            const vue2_path = await vue2BasicElementUI()
+            const vue2_path = await vue2BasicElementUI(custom)
             successMsg = [
                 `cd ${vue2_path}`,
                 'yarn 或者 npm install 安装依赖',
