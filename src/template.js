a6280f0cfe521a9a72f7e4b878c9c5f8cbb1406f
Gj <gongjie0422@163.com>
2021-03-23 11:05:13 +0800

    fix: temolate new vue2-components;

--- src/template.js
+++ src/template.js
@@ -23,6 +23,15 @@ function vue2BasicElementUI(custom) {
 }
 
 /**
+ * 下载vue2-element-ui-components基础组件库
+ */
+ function vue2BasicElementUIComponents(custom) {
+    const url = 'https://gitee.com:gjwork/vue2-basic-element-ui-components#master'
+    const dir = custom === '[default]' ? 'vue2-basic-element-ui-components' : custom
+    return util.DownloadTemplate(url, dir)
+}
+
+/**
  * 模板下载
  */
 exports.InitTemplate = async function () {
@@ -31,7 +40,11 @@ exports.InitTemplate = async function () {
             type: 'list',
             message: '模板选择',
             name: 'template',
-            choices: ["koa2-basic-template", "vue2-basic-element-ui-template"],
+            choices: [
+                "koa2-basic-template", 
+                "vue2-basic-element-ui-template", 
+                "vue2-basic-element-ui-components"
+            ],
         }, {
             type: 'input',
             message: '模板名称',
@@ -45,29 +58,26 @@ exports.InitTemplate = async function () {
     spinner.color = 'cyan';
     spinner.text = '正在安装...';
     const { template, custom } = data;
-    let successMsg = []
+    let firstMsg = ''
     // 下载koa模板
     switch (template) {
         case 'koa2-basic-template':
-            const koa2_path = await koaBasicServices(custom)
-            successMsg = [
-                `cd ${koa2_path}`,
-                'yarn 或者 npm install 安装依赖',
-                'yarn dev 启动开发环境'
-            ]
+            firstMsg = await koaBasicServices(custom)
             break;
         case 'vue2-basic-element-ui-template':
-            const vue2_path = await vue2BasicElementUI(custom)
-            successMsg = [
-                `cd ${vue2_path}`,
-                'yarn 或者 npm install 安装依赖',
-                'yarn serve 启动开发环境'
-            ]
+            firstMsg = await vue2BasicElementUI(custom)
+            break;
+        case 'vue2-basic-element-ui-components':
+            firstMsg = await vue2BasicElementUIComponents(custom)
             break;
         default:
             break;
     }
     spinner.succeed('安装完成!')
     spinner.stop()
-    print.Messages(successMsg)
+    print.Messages([
+        `cd ${firstMsg}`,
+        'yarn 或者 npm install 安装依赖',
+        'yarn serve 启动开发环境'
+    ])
 }
